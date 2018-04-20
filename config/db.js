068583899e36636e3d7cdb2afe55a277d67e6ef8
Narendra Maurya <nandymandy09@hotmail.com>
2018-04-21 04:44:41 +0530

    Little Mistake in database Config:

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI:'mongodb://<nandymandy>:<nandymandy>@ds151809.mlab.com:51809/vid-jot'}
+    module.exports = {mongoURI:'mongodb://nandymandy:nandymandy@ds151809.mlab.com:51809/vid-jot'}
 } else {
     module.exports = {mongoURI:'mongodb://127.0.0.1:27017/vidjot-dev'}
 }
